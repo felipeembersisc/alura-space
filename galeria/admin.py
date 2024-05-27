bc47b74e283abf80211b31b13a48fd034c075156
FelipePanizza <felipe.panizza@poupacred.com.br>
2024-05-27 11:18:42 -0300

    feat: add nova coluna na models fotografia + exibir fotografia na index

--- galeria/admin.py
+++ galeria/admin.py
@@ -3,8 +3,11 @@ from galeria.models import Fotografia
 
 
 class ListandoFotografias(admin.ModelAdmin):
-   list_display = ("id", "nome", "legenda")
+   list_display = ("id", "nome", "legenda", "publicada")
    list_display_links = ("id", "nome")
+   list_filter = ("categoria",)
+   list_editable = ("publicada",)
+   list_per_page = 10
    search_fields = ("nome",)
 
 admin.site.register(Fotografia, ListandoFotografias)
--- galeria/models.py
+++ galeria/models.py
@@ -9,11 +9,12 @@ class Fotografia(models.Model):
         ("PLANETA", "Planeta"),
     ]
 
-    nome      = models.CharField(max_length=100, null=False, blank=False)
-    legenda   = models.CharField(max_length=150, null=False, blank=False)
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    legenda = models.CharField(max_length=150, null=False, blank=False)
     categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIA, default="")
     descricao = models.TextField(null=False, blank=False)
-    foto      = models.CharField(max_length=100, null=False, blank=False)
+    foto = models.CharField(max_length=100, null=False, blank=False)
+    publicada = models.BooleanField(default=False)
 
     def __str__(self):
         return f"Fotografia [nome={self.nome}]"

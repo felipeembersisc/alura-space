17609ef2d14eb52fbcfc08bc9f3fb0ddb688243e
FelipePanizza <felipe.panizza@poupacred.com.br>
2024-05-27 13:55:49 -0300

    feat: criar app de usuários

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from galeria.views import index, imagem, buscar
 
+
 urlpatterns = [
     path('', index, name='index'),
     path('imagem/<int:foto_id>', imagem, name='imagem'),

e1bd2a1c44c499880fc698efb647f462d610b75e
FelipePanizza <felipe.panizza@poupacred.com.br>
2024-05-27 11:59:30 -0300

    feat: alterar o template para buscar as imagens no banco

--- setup/settings.py
+++ setup/settings.py
@@ -127,6 +127,11 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+# Media files
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+MEDIA_URL = "/media/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 

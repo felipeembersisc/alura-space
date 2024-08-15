95a5792f50eca5da488c7ba51a1b39adfa997b0f
FelipePanizza <felipe.panizza@poupacred.com.br>
2024-08-15 13:48:41 -0300

    feat: montar form de login

---
+++ usuarios/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+
+
+class LoginForms(forms.Form):
+    nome_login = forms.CharField(
+        label="Nome de login", 
+        required=True, 
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Ex: John Doe"
+            }
+        )
+    )
+    senha = forms.CharField(
+        label="Senha", 
+        required=True, 
+        max_length=70, 
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Digite sua senha"
+            }
+        )
+    )
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
+from usuarios.forms import LoginForms
+
 
 def login(request):
-   return render(request, 'usuarios/login.html')
+    form = LoginForms()
+    return render(request, "usuarios/login.html", {"form": form})
 
 
 def cadastro(request):
-    return render(request, 'usuarios/cadastro.html')
+    return render(request, "usuarios/cadastro.html")
